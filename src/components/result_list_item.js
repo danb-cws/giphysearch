e2fc16f6b2f27505eb28474253e6ed89f2ba9ecb
danb-cws <sugarlumpltd@gmail.com>
2018-06-16 19:26:00 +0100

    prettier

--- src/components/result_list_item.js
+++ src/components/result_list_item.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const ResultListItem = ({ gif, index, onItemSelect }) => {
   return (
-    <li onClick={() => {onItemSelect(index)}}>
+    <li
+      onClick={() => {
+        onItemSelect(index);
+      }}
+    >
       <h5>{gif.title}</h5>
       <img
         src={gif.images.fixed_height_small_still.url}
