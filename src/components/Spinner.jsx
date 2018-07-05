f431acea74aa43f8044f3658271d10b760a6a985
danb-cws <sugarlumpltd@gmail.com>
2018-07-05 12:33:45 +0100

    swapped spin logic

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Spinner = props => {
-  if (!props.isLoaded) {
-    return <p className="Spinner">spin spin spinner...</p>;
+  if (props.isLoaded) {
+    return <p className="Spinner">data loaded, spinner off..</p>;
   } else {
-    return <p className="Spinner">data loaded...</p>;
+    return <p className="Spinner">spin spin spinner on...</p>;
   }
 }
 export default Spinner;
