a2c0ba85aebee9f7d78955c967a73f71ac8f552c
danb-cws <sugarlumpltd@gmail.com>
2018-07-06 17:44:59 +0100

    wrap in a tag for better focus

--- src/components/ResultListItem.jsx
+++ src/components/ResultListItem.jsx
@@ -2,19 +2,21 @@ import React from "react";
 
 const ResultListItem = ({ gif, selectedItem, index, onItemSelect }) => {
   return (
-    <li
-      className={selectedItem === index ? "selected-item" : ""}
-      onClick={() => {
-        onItemSelect(index);
-      }}
-    >
-      <h5>{gif.title}</h5>
-      <img
-        src={gif.images.fixed_height_small_still.url}
-        width={gif.images.fixed_height_small_still.width}
-        height={gif.images.fixed_height_small_still.height}
-        alt={gif.title}
-      />
+    <li>
+      <a
+        className={selectedItem === index ? "selected-item" : null}
+        onClick={() => {
+          onItemSelect(index);
+        }}
+      >
+        <h5>{gif.title}</h5>
+        <img
+          src={gif.images.fixed_height_small_still.url}
+          width={gif.images.fixed_height_small_still.width}
+          height={gif.images.fixed_height_small_still.height}
+          alt={gif.title}
+        />
+      </a>
     </li>
   );
 };
