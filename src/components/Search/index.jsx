921044b42cceac5e0b0783a00f08cfcdb6b4577a
danb-cws <sugarlumpltd@gmail.com>
2018-07-11 19:01:06 +0100

    debounced search input

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import * as config from "../../config";
+import debounce from "debounce";
 import "./style.css";
 
 class Search extends Component {
@@ -8,21 +9,26 @@ class Search extends Component {
     this.state = { searchTerm: config.default_term };
   }
 
+  onInputChange = searchTerm => {
+    this.setState({ searchTerm });
+    this.props.onSearchTermChange(searchTerm);
+  };
+
+  debouncedOnInputChange = debounce(searchTerm => {
+    this.onInputChange(searchTerm);
+  }, 100); // any higher than 100 and it makes the controlled input very laggy
+
   render() {
     return (
       <input
         autoFocus
         className="SearchInput"
         value={this.state.searchTerm}
-        onChange={e => this.onInputChange(e.target.value)}
+        //onChange={e => this.onInputChange(e.target.value)}
+        onChange={e => this.debouncedOnInputChange(e.target.value)}
       />
     );
   }
-
-  onInputChange = searchTerm => {
-    this.setState({ searchTerm });
-    this.props.onSearchTermChange(searchTerm);
-  };
 }
 
 export default Search;
