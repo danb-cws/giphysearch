39ecddf6c2340ae9f0fa8ac0edfb8423f9b657ff
danb-cws <sugarlumpltd@gmail.com>
2018-07-12 13:53:24 +0100

    moved debouce on search input to stop lag on controlled input

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -11,12 +11,12 @@ class Search extends Component {
 
   onInputChange = searchTerm => {
     this.setState({ searchTerm });
-    this.props.onSearchTermChange(searchTerm);
+    this.debouncedOnInputChange(searchTerm);
   };
 
   debouncedOnInputChange = debounce(searchTerm => {
-    this.onInputChange(searchTerm);
-  }, 100); // any higher than 100 and it makes the controlled input very laggy
+    this.props.onSearchTermChange(searchTerm);
+  }, 250);
 
   render() {
     return (
@@ -24,8 +24,7 @@ class Search extends Component {
         autoFocus
         className="SearchInput"
         value={this.state.searchTerm}
-        //onChange={e => this.onInputChange(e.target.value)}
-        onChange={e => this.debouncedOnInputChange(e.target.value)}
+        onChange={e => this.onInputChange(e.target.value)}
       />
     );
   }
