2974ea562a6550a95eb7a12d8854160f24e87d2d
danb-cws <sugarlumpltd@gmail.com>
2018-07-16 19:10:58 +0100

    no results logic and stub of paginators

--- src/components/ResultList/index.jsx
+++ src/components/ResultList/index.jsx
@@ -3,6 +3,7 @@ import ResultListItem from "../ResultListItem";
 import "./style.css";
 
 const ResultList = props => {
+  const noResults = <li className="ResultList--no-results">No results</li>;
   const resultItems = props.gifs.map((gif, index) => {
     return (
       <ResultListItem
@@ -17,13 +18,17 @@ const ResultList = props => {
   });
   return (
     <div className="ResultList">
-      <ul className="ResultList--list">{resultItems}</ul>
+      <a className="ResultList--paginator">Back</a>
+      <ul className="ResultList--list">
+        {resultItems.length ? resultItems : noResults}
+      </ul>
       <a
+        className="ResultList--paginator"
         onClick={() => {
           props.onPaginate();
         }}
       >
-        next
+        Fwd
       </a>
     </div>
   );
