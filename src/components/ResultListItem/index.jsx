16438fbc5358c592b8fe41e4d5081cd561e0551f
danb-cws <sugarlumpltd@gmail.com>
2018-07-21 16:15:39 +0100

    setting a dataIndex (place in total results), setting selected class pass the gif.id to Player prettier

--- src/components/ResultListItem/index.jsx
+++ src/components/ResultListItem/index.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import "./style.css";
+import * as config from "../../config";
 
-const ResultListItem = ({ gif, selectedItem, index, onItemSelect, ChildNodeName }) => {
+const ResultListItem = ({
+  gif,
+  selectedItem,
+  index,
+  resultsPageIndex,
+  onItemSelect,
+  ChildNodeName
+}) => {
+  const dataIndex = resultsPageIndex * config.RESULTS_PER_PAGE + index;
+
   return (
     <ChildNodeName className="ResultList--item">
       <a
-        className={selectedItem === index ? "ResultList--item--link ResultList--item--link__selected-item" : "ResultList--item--link"}
+        className={
+          selectedItem === dataIndex
+            ? "ResultList--item--link ResultList--item--link__selected-item"
+            : "ResultList--item--link"
+        }
         onClick={() => {
-          onItemSelect(index);
+          if (selectedItem === dataIndex) {
+            return;
+          }
+          onItemSelect(index, gif.id);
         }}
       >
         <img
-          className='ResultList--item--img'
+          className="ResultList--item--img"
           width={gif.images.fixed_height_small_still.width}
           height={gif.images.fixed_height_small_still.height}
           src={gif.images.fixed_height_small_still.url}
           alt={gif.title}
         />
-        <h5 className="ResultList--item--title">{gif.title || '(No title)'}</h5>
+        <h5 className="ResultList--item--title">{gif.title || "(No title)"}</h5>
       </a>
     </ChildNodeName>
   );
