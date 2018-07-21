27d4a84350ce08758e253dbb4639d6fe3734e3e0
danb-cws <sugarlumpltd@gmail.com>
2018-07-21 16:27:04 +0100

    WIP persist loaded player choice

--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ class App extends Component {
   state = {
     searchTerm: config.default_term,
     gifs: [],
+    selectedGifData: {},
     selectedItem: 0,
     resultsPageIndex: 0,
     totalResults: 0,
+    currentId: 0,
     dataIsLoaded: false,
     imageIsLoaded: false
   };
@@ -23,17 +25,40 @@ class App extends Component {
     this.giphySearchHandler(this.state.searchTerm);
   }
 
+  setImageId(id) {
+    if (id === 0) {
+      this.setState({
+        currentId: id
+      });
+    } else {
+      this.setState({
+        currentId: id,
+        imageIsLoaded: false
+      });
+      this.fetchPlayerImage(id);
+    }
+  }
+
   giphySearchHandler(term) {
     this.setState({
       searchTerm: term,
       dataIsLoaded: false,
       imageIsLoaded: false
-    });
+    }, () => {
+      this.fetchData().catch(e => {console.log('error: ',e);});
+    }
+
+    )}
+
+  fetchData = () => {
+
+    console.log('hello!');
     return fetch(
-      `${config.GIPHY_ENDPOINT}${encodeURI(this.state.searchTerm)}&api_key=${
-        config.GIPHY_API_KEY
-      }&limit=${config.RESULTS_PER_PAGE}&offset=${this.state.resultsPageIndex *
-        config.RESULTS_PER_PAGE}`
+      `${config.GIPHY_ENDPOINT}search?q=${encodeURI(
+        this.state.searchTerm
+      )}&api_key=${config.GIPHY_API_KEY}&limit=${
+        config.RESULTS_PER_PAGE
+      }&offset=${this.state.resultsPageIndex * config.RESULTS_PER_PAGE}&rating=Y`
     )
       .then(response => {
         if (response.ok) {
@@ -49,13 +74,33 @@ class App extends Component {
         return json.data;
       })
       .then(gifs =>
-        this.setState({
-          gifs,
-          selectedItem: 0,
-          dataIsLoaded: true
-        })
+        this.setState(
+          {
+            gifs,
+            dataIsLoaded: true
+          },
+          gifs.length ? this.setImageId(gifs[this.state.selectedItem].id) : this.setImageId(0)
+        )
       )
       .catch(error => console.error(error));
+  }
+
+  fetchPlayerImage(id) {
+    return fetch(
+      `${config.GIPHY_ENDPOINT}${id}?&api_key=${config.GIPHY_API_KEY}`
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(response.statusText);
+        }
+      })
+      .then(json => {
+        this.setState({
+          selectedGifData: json.data
+        });
+      });
   }
 
   render() {
@@ -64,13 +109,13 @@ class App extends Component {
         <header className="App--header">
           <h1 className="App--title">Giphysearch</h1>
           <p>
-            A work in progress...{" "}
+            A work in progress...
             <a href="https://github.com/danb-cws/giphysearch">Github</a>
           </p>
         </header>
         <Search
           onSearchTermChange={searchTerm => {
-            this.setState({ searchTerm: searchTerm, resultsPageIndex: 0 });
+            this.setState({ searchTerm: searchTerm, resultsPageIndex: 0, selectedItem: 0 });
             this.giphySearchHandler(searchTerm);
           }}
         />
@@ -79,14 +124,19 @@ class App extends Component {
           selectedItem={this.state.selectedItem}
           totalResults={this.state.totalResults}
           resultsPageIndex={this.state.resultsPageIndex}
-          onItemSelect={index => {
-            if (index !== this.state.selectedItem) {
-              this.setState({ selectedItem: index, imageIsLoaded: false });
-            }
+          onItemSelect={(index, giphyId) => {
+            this.setState(
+              {
+                selectedItem: index,
+                imageIsLoaded: false
+              },
+              this.setImageId(giphyId)
+            );
           }}
           onPaginate={dir => {
             let nextPage = this.state.resultsPageIndex + dir;
             this.setState({ resultsPageIndex: nextPage }, () => {
+              console.log('TERM: ',this.state.searchTerm);
               this.giphySearchHandler(this.state.searchTerm);
             });
           }}
@@ -94,6 +144,8 @@ class App extends Component {
         <Spinner imageIsLoaded={this.state.imageIsLoaded} />
         <Player
           gifs={this.state.gifs}
+          selectedGifData={this.state.selectedGifData}
+          currentId={this.state.currentId}
           selectedItem={this.state.selectedItem}
           searchTerm={this.state.searchTerm}
           dataIsLoaded={this.state.dataIsLoaded}
--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -2,17 +2,26 @@ import React from "react";
 import "./style.css";
 
 const Player = props => {
-  const mainGif = props.gifs[props.selectedItem];
+  const mainGif = props.selectedGifData;
+
+  function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+
   if (props.gifs.length === 0 && props.dataIsLoaded) {
     return (
       <div className="Player">
         <div className="Player--errortext">
-          <div>{!props.searchTerm ? 'Enter a term in the search field' : `No gifs available for "${props.searchTerm}"`}</div>
+          <div>{!props.searchTerm ? 'Please enter a term in the search field' : `No gifs available for "${props.searchTerm}"`}</div>
         </div>
       </div>
     );
   }
-  if (!mainGif) {
+  if (isEmpty(mainGif)) {
     return <div className="Player Player--loadingtext">Loading json...</div>;
   }
 
