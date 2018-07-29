6efc22e1221b809100f61be09b81c97556bd55d2
DanB <sugarlumpltd@gmail.com>
2018-07-29 12:46:21 +0100

    copy tweak

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="App-header">
       <h1 className="App-header__title">Giphysearch</h1>
       <p className="App-header__intro">
-        A React demo by Dan B
+        A code demo by Dan B
         <a
           href="https://github.com/danb-cws/giphysearch"
           className="github-link"
