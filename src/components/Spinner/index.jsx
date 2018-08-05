a7c7323cffda0becdbc0aea8dfa58a5a71b9c605
danb-cws <sugarlumpltd@gmail.com>
2018-08-05 13:09:08 +0100

    destructuring out props

--- src/components/Spinner/index.jsx
+++ src/components/Spinner/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-const Spinner = props => {
-  return props.showSpinner ? <div className="Spinner" /> : null;
+const Spinner = ({showSpinner}) => {
+  return showSpinner ? <div className="Spinner" /> : null;
 };
 export default Spinner;
