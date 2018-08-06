43c680a2a74139c4e0895c0a38bee6ab066eacdd
danb-cws <sugarlumpltd@gmail.com>
2018-08-06 20:02:56 +0100

    refine Player to be selfcontained and solely reliant on currentId move Spinner into Player

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -1,44 +1,72 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
+import Spinner from "../Spinner/";
+import * as config from "../../config";
 
-const Player = props => {
-  const mainGif = props.selectedGifData;
+class Player extends Component {
+  state = { localCurrId: undefined, mainGif: {}, imageIsLoaded: true };
 
-  function isEmpty(obj) {
+  fetchPlayerImage = () => {
+    fetch(
+      `${config.GIPHY_ENDPOINT}${this.props.currentId}?&api_key=${
+        config.GIPHY_API_KEY
+      }`
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(response.statusText);
+        }
+      })
+      .then(json => {
+        this.setState({
+          localCurrId: this.props.currentId,
+          mainGif: json.data,
+          imageIsLoaded: false
+        });
+      })
+      .catch(() => console.error);
+  };
+
+  isEmpty = obj => {
     for (var key in obj) {
       if (obj.hasOwnProperty(key)) return false;
     }
     return true;
-  }
+  };
 
-  if (props.gifs.length === 0 && props.jsonIsLoaded) {
+  hideSpinner = () => {
+    this.setState({
+      imageIsLoaded: true
+    });
+  };
+
+  componentDidUpdate = () => {
+    if (
+      this.props.currentId !== 0 &&
+      this.props.currentId !== this.state.localCurrId
+    ) {
+      this.fetchPlayerImage(this.props.currentId);
+    }
+  };
+
+  render() {
+    if (this.isEmpty(this.state.mainGif)) {
+      return <div className="Player Player--loadingtext">?Loading json...</div>;
+    }
     return (
       <div className="Player">
-        <div className="Player--errortext">
-          <div>
-            {!props.searchTerm
-              ? "Please enter a term in the search field"
-              : `No gifs available for "${props.searchTerm}"`}
-          </div>
-        </div>
+        <Spinner showSpinner={!this.state.imageIsLoaded} />
+        <img
+          className="Player--img"
+          src={this.state.mainGif.images.downsized.url}
+          alt={this.state.mainGif.title}
+          onLoad={this.hideSpinner}
+        />
       </div>
     );
   }
-
-  if (isEmpty(mainGif)) {
-    return <div className="Player Player--loadingtext">Loading json...</div>;
-  }
-
-  return (
-    <div className="Player">
-      <img
-        className="Player--img"
-        src={mainGif.images.downsized.url}
-        alt={mainGif.title}
-        onLoad={props.onImageLoaded}
-      />
-    </div>
-  );
-};
+}
 
 export default Player;
