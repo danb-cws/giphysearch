4f54d47222ddaac395da123895a460f058987599
danb-cws <sugarlumpltd@gmail.com>
2018-08-06 20:11:00 +0100

    clean up Errorscreen

--- src/components/ErrorScreen/index.jsx
+++ src/components/ErrorScreen/index.jsx
@@ -2,15 +2,6 @@ import React from "react";
 import "./style.css";
 
 const ErrorScreen = props => {
-  const mainGif = props.selectedGifData;
-
-  function isEmpty(obj) {
-    for (var key in obj) {
-      if (obj.hasOwnProperty(key)) return false;
-    }
-    return true;
-  }
-
   if (props.gifs.length === 0 && props.jsonIsLoaded) {
     return (
       <div className="ErrorScreen">
@@ -21,10 +12,9 @@ const ErrorScreen = props => {
         </div>
       </div>
     );
-  }
-
-  if (isEmpty(mainGif)) {
-    return <div className="ErrorScreen">Loading json...</div>;
+  } else {
+    return null;
   }
 };
+
 export default ErrorScreen;
