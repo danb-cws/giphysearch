734d8cf090d94246b9c4ca8399dafd103aee0017
danb-cws <sugarlumpltd@gmail.com>
2018-08-09 18:14:18 +0100

    prettier

--- src/components/Spinner/Spinner.test.js
+++ src/components/Spinner/Spinner.test.js
@@ -8,12 +8,11 @@ configure({ adapter: new Adapter() });
 describe("Spinner tests", () => {
   it("Shows spinner if showSpinner is true", () => {
     const el = shallow(<Spinner showSpinner={true} />);
-
     expect(el.contains(<div className="Spinner" />)).toBe(true);
   });
+
   it("Renders nothing if showSpinner is false", () => {
     const el = shallow(<Spinner showSpinner={false} />);
-
     expect(el.find(Spinner).exists()).toEqual(false);
   });
 });
