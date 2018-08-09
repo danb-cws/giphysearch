0b4bed63698c5bfd00065b600dd12df6e9febc98
danb-cws <sugarlumpltd@gmail.com>
2018-08-09 12:18:10 +0100

    remove redundant div

--- src/components/ErrorScreen/index.jsx
+++ src/components/ErrorScreen/index.jsx
@@ -5,11 +5,9 @@ const ErrorScreen = props => {
   if (props.gifs.length === 0 && props.jsonIsLoaded) {
     return (
       <div className="ErrorScreen">
-        <div>
           {!props.searchTerm
             ? "Please enter a term in the search field"
             : `No gifs available for "${props.searchTerm}"`}
-        </div>
       </div>
     );
   } else {
