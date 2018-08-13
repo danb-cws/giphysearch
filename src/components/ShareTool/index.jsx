afb0635a49b365b99e2d5e97be3c8e6d37296f1a
DanB <sugarlumpltd@gmail.com>
2018-08-14 00:56:41 +0100

    prettier

--- src/components/ShareTool/index.jsx
+++ src/components/ShareTool/index.jsx
@@ -4,7 +4,7 @@ import "./style.css";
 class ShareTool extends Component {
   state = {
     isShowing: false,
-    copySuccess: ''
+    copySuccess: ""
   };
 
   showTool = () => {
@@ -15,25 +15,34 @@ class ShareTool extends Component {
     this.setState({ isShowing: false });
   };
 
-  copyToClipboard = (e) => {
+  copyToClipboard = e => {
     this.textArea.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     e.target.focus();
-    this.setState({ copySuccess: 'Copied!' });
+    this.setState({ copySuccess: "Copied!" });
   };
 
   render() {
     return !this.state.isShowing ? (
-      <button className="ShareTool--open-button" onClick={this.showTool}></button>
+      <button className="ShareTool--open-button" onClick={this.showTool} />
     ) : (
       <section className="ShareTool--panel">
         <p>Embed url:</p>
-        <textarea className="ShareTool--textarea" ref={(textarea) => this.textArea = textarea}>{this.props.shareUrl}</textarea>
-        {document.queryCommandSupported('copy') &&
-        <button className="ShareTool--button" onClick={this.copyToClipboard}>copy url to clipboard</button>
-        }
+        <textarea
+          className="ShareTool--textarea"
+          ref={textarea => (this.textArea = textarea)}
+        >
+          {this.props.shareUrl}
+        </textarea>
+        {document.queryCommandSupported("copy") && (
+          <button className="ShareTool--button" onClick={this.copyToClipboard}>
+            Copy url to clipboard
+          </button>
+        )}
         <p>{this.state.copySuccess}</p>
-        <button className="ShareTool--button" onClick={this.hideTool}>close</button>
+        <button className="ShareTool--button" onClick={this.hideTool}>
+          Close
+        </button>
       </section>
     );
   }
