c86e538e18c96fa9b9459c7c68a98f320eb7af1c
danb-cws <sugarlumpltd@gmail.com>
2018-08-14 13:40:51 +0100

    add ios fix for 'copy to clipboard'

--- src/components/ShareTool/index.jsx
+++ src/components/ShareTool/index.jsx
@@ -22,7 +22,34 @@ class ShareTool extends Component {
   };
 
   copyToClipboard = e => {
-    this.textArea.select();
+    // handle iOS as a special case
+    if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+      // save current contentEditable/readOnly status
+      const el = this.textArea;
+      var editable = el.contentEditable;
+      var readOnly = el.readOnly;
+
+      // convert to editable with readonly to stop iOS keyboard opening
+      el.contentEditable = true;
+      el.readOnly = true;
+
+      // create a selectable range
+      var range = document.createRange();
+      range.selectNodeContents(el);
+
+      // select the range
+      var selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+      el.setSelectionRange(0, 999999);
+
+      // restore contentEditable/readOnly to original state
+      el.contentEditable = editable;
+      el.readOnly = readOnly;
+    } else {
+      this.textArea.select();
+    }
+
     document.execCommand("copy");
     e.target.focus();
     this.setState({ copySuccess: "Copied!" });
@@ -39,8 +66,7 @@ class ShareTool extends Component {
             className="ShareTool--textarea"
             ref={textarea => (this.textArea = textarea)}
             defaultValue={this.props.shareUrl}
-          >
-          </textarea>
+          />
           {document.queryCommandSupported("copy") && (
             <button
               className="ShareTool--button"
