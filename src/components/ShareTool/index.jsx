7080721d639898c1746777f0f92bd95ea9f5bac8
danb-cws <sugarlumpltd@gmail.com>
2018-08-14 10:28:43 +0100

    more share tool panel css

--- src/components/ShareTool/index.jsx
+++ src/components/ShareTool/index.jsx
@@ -11,6 +11,12 @@ class ShareTool extends Component {
     this.setState({ isShowing: true });
   };
 
+  closeTool = e => {
+    if (e.target.classList.contains("modal")) {
+      this.hideTool();
+    }
+  };
+
   hideTool = () => {
     this.setState({ isShowing: false });
   };
@@ -26,24 +32,29 @@ class ShareTool extends Component {
     return !this.state.isShowing ? (
       <button className="ShareTool--open-button" onClick={this.showTool} />
     ) : (
-      <section className="ShareTool--panel">
-        <p>Embed url:</p>
-        <textarea
-          className="ShareTool--textarea"
-          ref={textarea => (this.textArea = textarea)}
-        >
-          {this.props.shareUrl}
-        </textarea>
-        {document.queryCommandSupported("copy") && (
-          <button className="ShareTool--button" onClick={this.copyToClipboard}>
-            Copy url to clipboard
+      <div className="modal" onClick={this.closeTool}>
+        <section className="ShareTool--panel">
+          <h3>Embed url:</h3>
+          <textarea
+            className="ShareTool--textarea"
+            ref={textarea => (this.textArea = textarea)}
+          >
+            {this.props.shareUrl}
+          </textarea>
+          {document.queryCommandSupported("copy") && (
+            <button
+              className="ShareTool--button"
+              onClick={this.copyToClipboard}
+            >
+              Copy url to clipboard
+            </button>
+          )}
+          <p>{this.state.copySuccess}</p>
+          <button className="ShareTool--button" onClick={this.hideTool}>
+            Close
           </button>
-        )}
-        <p>{this.state.copySuccess}</p>
-        <button className="ShareTool--button" onClick={this.hideTool}>
-          Close
-        </button>
-      </section>
+        </section>
+      </div>
     );
   }
 }
