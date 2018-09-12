39f5e9d6331a63cf8970ee7662097f4fd4e4cd14
danb-cws <sugarlumpltd@gmail.com>
2018-09-12 17:17:58 +0100

    search snapshot test and onchange test

--- src/components/Search/test.js
+++ src/components/Search/test.js
@@ -3,25 +3,22 @@ import { mount } from "enzyme";
 import Search from "./";
 
 const el = mount(
-  <Search
-    onSearchTermChange={searchTerm => {
-      this.setState(
-        {
-          searchTerm,
-          resultsPageIndex: 0,
-          selectedItemIndex: 0,
-          hasPaginated: false
-        },
-        () => {
-          this.giphySearchHandler();
-        }
-      );
-    }}
-  />
+  <Search />
 );
 
 describe("Search tests", () => {
   it("Renders the Search component", () => {
     expect(el.exists()).toBe(true);
+  });
+
+  it("Renders and matches snapshot", () => {
+    expect(el).toMatchSnapshot();
+  });
+
+  it("Responds to onChange on the input", () => {
+    el.find('input').simulate("change", {
+      target: { value: "hello" }
+    });
+    expect(el.state("searchTerm")).toEqual("hello");
   });
 });
