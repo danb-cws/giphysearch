717164f2be4d1d7d2dac5b39043acb2cc2e6ee49
danb-cws <sugarlumpltd@gmail.com>
2018-09-12 14:25:22 +0100

    tidy snapshot test

--- src/components/Spinner/test.js
+++ src/components/Spinner/test.js
@@ -4,6 +4,12 @@ import renderer from "react-test-renderer";
 import Spinner from "./";
 
 describe("Spinner tests", () => {
+  it("Renders and matches snapshot", () => {
+    const tree = renderer.create(<Spinner showSpinner={true} />)
+    .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
   it("Shows spinner if showSpinner is true", () => {
     const el = shallow(<Spinner showSpinner={true} />);
     expect(el.contains(<div className="Spinner" />)).toBe(true);
@@ -11,12 +17,6 @@ describe("Spinner tests", () => {
 
   it("Renders nothing if showSpinner is false", () => {
     const el = shallow(<Spinner showSpinner={false} />);
-    expect(el.find(Spinner).exists()).toEqual(false);
-  });
-
-  it("Renders and matches snapshot", () => {
-    const component = renderer.create(<Spinner showSpinner={true} />);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(el.find(Spinner).exists()).toBe(false);
   });
 });
