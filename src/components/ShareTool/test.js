5f77cc5de6d6dcd842661c8a10684d46f8dc0cf3
danb-cws <sugarlumpltd@gmail.com>
2018-09-12 16:47:32 +0100

    sharetool snapshot tests for open state and copy url

--- src/components/ShareTool/test.js
+++ src/components/ShareTool/test.js
@@ -1,12 +1,50 @@
 import React from "react";
-import {  shallow } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 import ShareTool from "./index";
-
-const el = shallow(<ShareTool />);
+import testjson from "mock_json/gifMock.json";
 
 describe("ShareTool tests", () => {
   it("Renders the ShareTool component", () => {
+    const el = shallow(<ShareTool shareUrl={testjson.embed_url} />);
     expect(el.exists()).toBe(true);
   });
 
+  it("Renders and matches snapshot (closed state)", () => {
+    const tree = shallow(<ShareTool shareUrl={testjson.embed_url} />);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Renders and matches snapshot (open state)", () => {
+    global.document.queryCommandSupported = () => {
+      return {
+        copy: () => {
+          true;
+        }
+      };
+    };
+    const tree = mount(<ShareTool shareUrl={testjson.embed_url} />);
+    tree.find("button.ShareTool--open-button").simulate("click");
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("Copies to clipboard", () => {
+    global.document.queryCommandSupported = () => {
+      return {
+        copy: () => {
+          true;
+        }
+      };
+    };
+    global.document.execCommand = () => {
+      return {
+        copy: () => {
+          true;
+        }
+      };
+    };
+    const tree = mount(<ShareTool shareUrl={testjson.embed_url} />);
+    tree.find("button.ShareTool--open-button").simulate("click");
+    tree.find("button.ShareTool--copy-button").simulate("click");
+    expect(tree).toMatchSnapshot();
+  });
 });
