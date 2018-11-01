574c42274f01b85fa1ab7340a1e992cac14901a1
danb-cws <sugarlumpltd@gmail.com>
2018-11-01 19:12:00 +0000

    render with mock json to reflect change to prevent redundant fetch in Player

--- src/components/Player/test.js
+++ src/components/Player/test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Player from "./";
+import testjson from "mock_json/gifMock.json";
 
-const el = shallow(<Player currentId={"JIX9t2j0ZTN9S"} />);
+const el = shallow(<Player currentChoiceData={testjson} />);
 
 describe("Player tests", () => {
   it("Renders the Player component", () => {
