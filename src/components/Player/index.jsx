56ea7f7f724a284991a1bf882e4c84c973032355
danb-cws <sugarlumpltd@gmail.com>
2018-11-01 19:04:54 +0000

    rework to remove redundant fetch

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -2,33 +2,9 @@ import React, { Component } from "react";
 import "./style.css";
 import Spinner from "../Spinner/";
 import ShareTool from "../ShareTool/";
-import * as config from "config";
-import debounce from "debounce";
 
 class Player extends Component {
-  state = { mainGif: {}, imageIsLoaded: undefined };
-
-  fetchPlayerImage = () => {
-    fetch(
-      `${config.GIPHY_ENDPOINT}${this.props.currentId}?&api_key=${
-        config.GIPHY_API_KEY
-      }`
-    )
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          //throw new Error(response.statusText);
-          console.warn('died! ',response.statusText);
-        }
-      })
-      .then(json => {
-        this.setState({
-          mainGif: json.data
-        });
-      })
-      .catch(() => console.error);
-  };
+  state = { imageIsLoaded: true };
 
   isEmpty = obj => {
     for (var key in obj) {
@@ -37,7 +13,7 @@ class Player extends Component {
     return true;
   };
 
-  hideSpinner = () => {
+  imgLoaded = () => {
     this.setState({
       imageIsLoaded: true
     });
@@ -45,44 +21,33 @@ class Player extends Component {
 
   componentDidUpdate(prevProps) {
     if (
-      this.props.currentId !== 0 && // ie. not 'no term' or 'no results'
-      this.props.currentId !== prevProps.currentId // prevent the fetch on every render if id unchanged
+      !this.isEmpty(this.props.currentChoiceData) && // ie. not 'no term' or 'no results'
+      this.props.currentChoiceData !== prevProps.currentChoiceData // prevent the render if data unchanged
     ) {
-      this.debouncedFetch(this.props.currentId);
+      this.setState({
+        imageIsLoaded: false
+      });
     }
   }
 
-  debouncedFetch = debounce(id => {
-    this.setState(
-      {
-        mainGif: {},
-        imageIsLoaded: false
-      },
-      this.fetchPlayerImage(id)
-    );
-  }, 400);
-
   render() {
-    const { mainGif, imageIsLoaded } = this.state;
-    if (this.props.currentId === (0 || undefined)) {
+    const { imageIsLoaded } = this.state;
+    const gifData = this.props.currentChoiceData;
+    if (this.isEmpty(gifData)) {
       return null;
     } else
       return (
         <section className="Player">
           <Spinner showSpinner={!imageIsLoaded} />
-          {this.isEmpty(mainGif) ? (
-            "Loading json..."
-          ) : (
-            <React.Fragment>
-              <ShareTool shareUrl={mainGif.embed_url} />
-              <img
-                className="Player--img"
-                src={mainGif.images.downsized.url}
-                alt={mainGif.title}
-                onLoad={this.hideSpinner}
-              />
-            </React.Fragment>
-          )}
+          <React.Fragment>
+            <ShareTool shareUrl={gifData.embed_url} />
+            <img
+              className="Player--img"
+              src={gifData.images.downsized.url}
+              alt={gifData.title}
+              onLoad={this.imgLoaded}
+            />
+          </React.Fragment>
         </section>
       );
   }
