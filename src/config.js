b6aff42687f0add0cc1f8f72a636c4d33c87d89a
Dan Brown <sugarlumpltd@gmail.com>
2019-05-16 23:06:59 +0100

    change key

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const GIPHY_API_KEY = "dc6zaTOxFJmzC";
+export const GIPHY_API_KEY = "OSWleZiTZ7uBqSAmNK9JP5BWzoSNtuq9";
 export const GIPHY_ENDPOINT = "https://api.giphy.com/v1/gifs/";
 export const RESULTS_PER_PAGE = 6;
 export const default_term = "";
